85ccee785067e9fba2bf0c3ff2c0ba4b0fd53957
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-07-20 23:27:44 +0900

    sc-service

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,21 @@
 $(function(){
+  // sc-video play
   const video = $('.sc-video video').get(0);
 
   $('.sc-video .btn-control').click(function(e){
     e.preventDefault();
     $(this).toggleClass('pause');
     if ($(this).hasClass('pause')){
-     video.play();
+      video.play();
     } else{
       video.pause();
     }
+  });
+
+  // sc-service color-box 
+  $('.sc-service .btn-color').click(function(e){
+    e.preventDefault();
+    $('.sc-service .btn-color').removeClass('active');
+    $(this).addClass('active');
   });
 });
