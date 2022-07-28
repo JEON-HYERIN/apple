f755d4918ff8aa6d37333a84881a5b8852b8bfc3
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-07-28 23:50:19 +0900

    0728수정

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,6 +4,97 @@ $(function () {
     e.preventDefault();
   });
 
+  $(window).scroll(function(){
+    const currentScroll = $(this).scrollTop();
+
+    const contentNav = $('.content-nav').offset().top;
+    const scDesign = $('.sc-design').offset().top;
+
+    if(currentScroll >= contentNav) {
+      $('.content-nav').addClass('fixed');
+    } else {
+      $('.content-nav').removeClass('fixed');
+    }
+
+    if(currentScroll >= scDesign - $(window).height()) {
+      $('.sc-video .video-area').addClass('sticky');
+    } else {
+      $('.sc-video .video-area').removeClass('sticky');
+    }
+  })
+  
+  // 전체영역 페이드 주면서 나오게 함
+  const infoListFade = gsap.fromTo('.sc-video .info-list', {
+    opacity: 0
+  }, {
+    opacity: 0.99
+  })
+
+  // 트리거 분할
+  ScrollTrigger.create({
+    trigger: '.sc-video .info-list',
+    start: 'top center',
+    end: '10% 20%',
+    // markers: true,
+    scrub: 1,
+    animation: infoListFade
+  })
+
+  $('.sc-video .info-item').each(function(index, el){
+    ScrollTrigger.create({
+      trigger: el,
+      start: 'top 60%',
+      end: 'bottom 30%',
+      // markers: true,
+      // scrub: 1,
+      toggleClass: {targets: el, className: "active"}
+    })
+  });
+
+  // 헤드셋 움직이게
+  // .sc-design .thumb-area-wrap
+  // .sc-design .thumb-box.frame
+  // .sc-design .thumb-box.cushion
+
+  gsap.to('.sc-design .thumb-area-wrap', {
+    scrollTrigger: {
+      trigger: '.sc-design .design-inner1',
+      start: 'top 100px',
+      end: 'bottom -10%',
+      // markers: true,
+      scrub: 1,
+    },
+    scale: 1.1
+  })
+
+  const headsetMotion = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.sc-design .design-inner1',
+      start: 'top -30%',
+      end: 'bottom -10%',
+      // markers: true,
+      scrub: 1,
+    },
+  })
+  headsetMotion.addLabel('motion1')
+  .to('.sc-design .thumb-box.frame', {yPercent: -3}, 'motion1+=0.4')
+  .to('.sc-design .thumb-box.cushion', {yPercent: 3}, 'motion1+=0.4')
+
+  $('[data-target]').each(function(index, el){
+    ScrollTrigger.create({
+      trigger: el,
+      start: 'top 60%',
+      end: 'bottom -300%',
+      // markers: true,
+      // scrub: 1,
+      toggleClass: {targets: el, className: "active"}
+    })
+  });
+  
+  // .fromTo('[data-target="text1"]', {opacity: 0},{opacity: 1},'motion1+=0.1')
+  // .fromTo('[data-target="text2"]', {opacity: 0},{opacity: 1},'motion1+=0.2')
+  // .fromTo('[data-target="text3"]', {opacity: 0},{opacity: 1},'motion1+=0.3')
+
   // video play
   const video = $('.sc-video video').get(0);
   const crownVideo = $('.sc-design .crown-video').get(0);
@@ -34,18 +125,49 @@ $(function () {
     $(this).addClass('active');
   });
 
-  // swiper
-  new Swiper('.sc-design .gallery-swiper', {
+ // swiper
+  const colorSlide1 = new Swiper('.sc-design .gallery-swiper1', {
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1500,
+  loop: true,
+  pagination: {
+    el: '.sc-design .swiper-pagination',
+    clickable: true,
+  },
+});
+
+  const colorSlide2= new Swiper('.sc-design .gallery-swiper2', {
+    effect:'fade',
     autoplay: {
       delay: 5000,
     },
     loop: true,
-
     pagination: {
       el: '.sc-design .swiper-pagination',
       clickable: true,
     },
   });
+
+colorSlide1.on('slideChange', function () {
+  idx = colorSlide1.realIndex;
+
+  colorSlide2.slideToLoop(idx);
+});
+
+$('.sc-design .slide-control').click(function(e){
+  e.preventDefault();
+  if ($(this).hasClass('pause')) {
+    colorSlide1.autoplay.stop();
+    colorSlide2.autoplay.stop();
+    $(this).removeClass('pause');
+  } else {
+    colorSlide1.autoplay.start();
+    colorSlide2.autoplay.start();
+    $(this).addClass('pause');
+  }
+});
 
   // gsap
   gsap.fromTo('.sc-intro .thumb-box img', {
@@ -76,20 +198,35 @@ $(function () {
     duration: 1.2
   });
 
-  // gsap.fromTo('.sc-design .group-quality .column-left .thumb-box img', {
-  // }, {
-  //     scrollTrigger: {
-  //     trigger: '.sc-design .group-quality .quality-area',
-  //     start: '0% 0%', 
-  //     // end: '100% 50%',
-  //     scrub: true,
-  //     pin: true,
-  //   },
-  //   scaleX: 1,
-  //   scaleY: 1,
-  //   y: -84,
-  //   duration: .6
-  // });
+  gsap.fromTo('.sc-design .thumb-comfort img', {
+  }, {
+      scrollTrigger: {
+      trigger: '.sc-design .thumb-comfort img',
+      start: '0% 100%', 
+      end: '100% 50%',
+      scrub: 1,
+      // pin: true,
+    },
+    scaleX: 1,
+    scaleY: 1,
+    yPercent: -10,
+    duration: .6
+  });
+  
+  gsap.fromTo('.sc-audio .thumb-quality img', {
+  }, {
+      scrollTrigger: {
+      trigger: '.sc-audio .thumb-quality img',
+      start: '0% 100%', 
+      end: '100% 50%',
+      scrub: 1,
+      // pin: true,
+    },
+    scaleX: 1,
+    scaleY: 1,
+    yPercent: -10,
+    duration: .6
+  });
 
   // gsap.fromTo('.sc-audio .group-quality .column-left .thumb-box img', {
   // }, {
